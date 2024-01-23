682824c8dcb5cba6e8c5778054f100d44aa82701
E-K8 <ekatplat@gmail.com>
2024-01-23 21:48:44 +0000

    fix reset by updating the displayed time with useEffect

--- 25plus5/src/TimeSetter.tsx
+++ 25plus5/src/TimeSetter.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import { FaArrowDown, FaArrowUp } from 'react-icons/fa';
 
 interface TimerSetterProps {
@@ -17,17 +18,27 @@ const TimeSetter: React.FC<TimerSetterProps> = ({
   interval,
   type,
 }) => {
+  const [displayTime, setDisplayTime] = useState(time);
+
+  useEffect(() => {
+    setDisplayTime(time);
+  }, [time]);
+
   return (
     <div>
       <button
-        onClick={() => (time > min ? setTime(time - interval) : null)}
+        onClick={() =>
+          displayTime > min ? setTime(displayTime - interval) : null
+        }
         id={`${type}-decrement`}
       >
         <FaArrowDown />
       </button>
-      <span id={`${type}-length`}>{time / interval}</span>
+      <span id={`${type}-length`}>{displayTime / interval}</span>
       <button
-        onClick={() => (time < max ? setTime(time + interval) : null)}
+        onClick={() =>
+          displayTime < max ? setTime(displayTime + interval) : null
+        }
         id={`${type}-increment`}
       >
         <FaArrowUp />
